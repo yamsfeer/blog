1f88025fb93cd1cb91ca3b9f2f2ea725a689deb8
yams <feer.yams@gmail.com>
2021-01-21 16:13:26 +0800

    开启主题博客主页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,5 @@
-module.exports = {
+const { config } = require("vuepress-theme-hope");
+module.exports = config({
   // 发布到github page后路径需要有前缀'blog'
   // 应当总是以斜杠开始，并以斜杠结束
   // 一个 base 路径一旦被设置，它将会自动地作为前缀插入到 .vuepress/config.js 中所有以 / 开始的资源路径中
@@ -19,9 +20,9 @@ module.exports = {
     displayAllHeaders: true,
     searchMaxSuggestions: 10,
     // theme-hope
-    blog: false,
     mdEnhance: {
       tex: true,
+      flowchart: true,
     },
     nav: [
       {
@@ -138,4 +139,4 @@ module.exports = {
     },
     smoothScroll: true
   }
-}
+})
--- docs/README.md
+++ docs/README.md
@@ -5,7 +5,9 @@ heroText: yams的博客
 tagline: null
 actionText: null
 actionLink: null
-search: true
 navbar: true
 # footer: MIT Licensed | Copyright © 2019-present melogra
+
+# theme-hope
+blog: true
 ---
