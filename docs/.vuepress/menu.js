0c14005c68920cc00903ce56d9ed2d0dbc78fe01
yamsfeer <feer.yams@gmail.com>
2022-07-04 23:56:54 +0800

    发布《games101笔记--光栅化部分》

--- docs/.vuepress/menu.js
+++ docs/.vuepress/menu.js
@@ -1,7 +1,7 @@
 const { readdirSync } = require('fs')
 const path = require('path')
 
-function getSidebar(dirname) {
+function dir(dirname) {
   let files = readdirSync(path.resolve(`${__dirname}/../${dirname}`))
   return files.map(filename => {
     return {
@@ -23,10 +23,24 @@ const navbar = [
       { text: '计算机网络', link: '/计算机基础/计算机网络/' },
     ]
   },
-  { text: '数学', link: '/数学/线性代数的本质/0.序言.md' },
+  {
+    text: '数学',
+    children: [
+      { text: '线性代数', link: '/数学/线性代数的本质/0.序言.md' },
+      { text: '几何', link: '/数学/几何/欧几里得几何与非欧几何.md' },
+      { text: '微积分', link: '/数学/微积分/微积分基本概念.md' },
+      { text: '傅里叶变换', link: '/数学/傅里叶变换/傅里叶变换.md' },
+    ]
+  },
   { text: '算法', link: '/算法设计与分析/开始.md' },
   { text: '源码', link: '/源码/vue/1. 准备工作.md' },
-  { text: '图形学', link: '/图形学/现代计算机图形学入门/前言.md' },
+  {
+    text: '图形学',
+    children: [
+      { text: 'games101', link: '/图形学/现代计算机图形学入门/0.序言.md' },
+      { text: '图形学API', link: '/图形学/图形学API/webGL.md' },
+    ]
+  },
   {
     text: '前端',
     children: [
@@ -40,151 +54,57 @@ const navbar = [
         ]
       },
       { text: 'css', link: '/css世界/1.块级元素与width、height.md' },
+      { text: '工程化', link: '/工程化/工具链.md' },
     ]
   },
-  { text: '工程化', link: '/工程化/工具链.md' },
   { text: 'clutter', link: '/clutter/网址收藏.md' },
 ];
 
 const sidebar = {
-  '/clutter/': [
-    {
-      text: 'clutter',
-      children: getSidebar('clutter')
-    }
-  ],
-  '/YDKJS(上卷)/': [
-    {
-      text: '你不知道的JS(上卷)',
-      children: getSidebar('YDKJS(上卷)')
-    }
-  ],
-  '/YDKJS(中卷)/': [
-    {
-      text: '你不知道的JS(中卷)',
-      children: getSidebar('YDKJS(中卷)')
-    }
-  ],
-  '/YDKJS(下卷)/': [
-    {
-      text: '你不知道的JS(下卷)',
-      children: getSidebar('YDKJS(下卷)')
-    }
-  ],
+  '/clutter/': [{ text: 'clutter', children: dir('clutter') }],
+
+  '/YDKJS(上卷)/': [{ text: '你不知道的JS(上卷)', children: dir('YDKJS(上卷)') }],
+  '/YDKJS(中卷)/': [{ text: '你不知道的JS(中卷)', children: dir('YDKJS(中卷)') }],
+  '/YDKJS(下卷)/': [{ text: '你不知道的JS(下卷)', children: dir('YDKJS(下卷)') }],
+
   '/算法设计与分析/': [
-    {
-      text: '开始',
-      children: [{ text: '开始', link: '/算法设计与分析/开始.md' }]
-    },
-    {
-      text: '分治',
-      prefix: '分治/',
-      children: getSidebar('算法设计与分析/分治')
-    },
-    {
-      text: '动态规划',
-      prefix: '动态规划/',
-      children: getSidebar('算法设计与分析/动态规划')
-    },
-    {
-      text: '贪心',
-      prefix: '贪心/',
-      children: getSidebar('算法设计与分析/贪心')
-    },
-    {
-      text: '回溯',
-      prefix: '回溯/',
-      children: getSidebar('算法设计与分析/回溯')
-    },
-    {
-      text: '分支限界',
-      prefix: '分支限界/',
-      children: getSidebar('算法设计与分析/分支限界')
-    },
-    {
-      text: '总结',
-      children: [{ text: '总结', link: '/算法设计与分析/总结.md' }]
-    },
+    { text: '开始', children: [{ text: '开始', link: '/算法设计与分析/开始.md' }] },
+    { text: '分治', prefix: '分治/', children: dir('算法设计与分析/分治') },
+    { text: '动态规划', prefix: '动态规划/', children: dir('算法设计与分析/动态规划') },
+    { text: '贪心', prefix: '贪心/', children: dir('算法设计与分析/贪心') },
+    { text: '回溯', prefix: '回溯/', children: dir('算法设计与分析/回溯') },
+    { text: '分支限界', prefix: '分支限界/', children: dir('算法设计与分析/分支限界') },
+    { text: '总结', children: [{ text: '总结', link: '/算法设计与分析/总结.md' }] },
   ],
 
-  '/计算机基础/计算机科学': [
-    {
-      text: '计算机科学',
-      children: getSidebar('计算机基础/计算机科学')
-    },
-  ],
-  '/计算机基础/计算机体系结构': [
-    {
-      text: '计算机体系结构',
-      children: getSidebar('计算机基础/计算机体系结构')
-    },
-  ],
-  '/计算机基础/汇编语言': [
-    {
-      text: '汇编语言',
-      children: getSidebar('计算机基础/汇编语言')
-    },
-  ],
-  '/计算机基础/操作系统': [
-    {
-      text: '操作系统',
-      children: getSidebar('计算机基础/操作系统')
-    },
-  ],
-  '/计算机基础/编译原理': [
-    {
-      text: '编译原理',
-      children: getSidebar('计算机基础/编译原理')
-    },
-  ],
-  '/计算机基础/计算机网络': [
-    {
-      text: '计算机网络',
-      children: getSidebar('计算机基础/计算机网络')
-    },
-  ],
-  '/数学/': [
-    {
-      text: '线性代数的本质',
-      prefix: '线性代数的本质',
-      children: getSidebar('数学/线性代数的本质')
-    }
-  ],
-  '/图形学/': [
-    {
-      text: '现代计算机图形学入门',
-      prefix: '现代计算机图形学入门',
-      children: getSidebar('图形学/现代计算机图形学入门')
-    },
-    {
-      text: '图形学API入门',
-      prefix: '图形学API入门',
-      children: getSidebar('图形学/图形学API入门')
-    }
-  ],
+  '/计算机基础/计算机科学': [{ text: '计算机科学', children: dir('计算机基础/计算机科学') }],
+  '/计算机基础/计算机体系结构': [{ text: '计算机体系结构', children: dir('计算机基础/计算机体系结构') }],
+  '/计算机基础/汇编语言': [{ text: '汇编语言', children: dir('计算机基础/汇编语言') }],
+  '/计算机基础/操作系统': [{ text: '操作系统', children: dir('计算机基础/操作系统') }],
+  '/计算机基础/编译原理': [{ text: '编译原理', children: dir('计算机基础/编译原理') }],
+  '/计算机基础/计算机网络': [{ text: '计算机网络', children: dir('计算机基础/计算机网络') }],
+
+  '/数学/线性代数的本质/': [{ text: '线性代数的本质', children: dir('数学/线性代数的本质') }],
+  '/数学/几何/': [{ text: '几何', children: dir('数学/几何') }],
+  '/数学/微积分/': [{ text: '微积分', children: dir('数学/微积分') }],
+  '/数学/傅里叶变换/': [{ text: '傅里叶变换', children: dir('数学/傅里叶变换') }],
+
+  '/图形学/现代计算机图形学入门': [{ text: '现代计算机图形学入门', children: dir('图形学/现代计算机图形学入门') }],
+  '/图形学/图形学API': [{ text: '图形学API', children: dir('图形学/图形学API') }],
+
   '/浏览器': [
     {
       text: 'webkit技术内幕',
       prefix: 'webkit技术内幕',
-      children: getSidebar('浏览器/webkit技术内幕')
+      children: dir('浏览器/webkit技术内幕')
     },
     {
       text: '浏览器',
-      children: getSidebar('浏览器')
+      children: dir('浏览器')
     }
   ],
-  '/源码/vue': [
-    {
-      text: 'vue',
-      children: getSidebar('源码/vue')
-    }
-  ],
-  '/css世界/': [
-    {
-      text: 'css',
-      children: getSidebar('css世界')
-    }
-  ]
+  '/源码/vue': [{ text: 'vue', children: dir('源码/vue') }],
+  '/css世界/': [{ text: 'css', children: dir('css世界') }]
 };
 
 module.exports = {
